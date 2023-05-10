7db36aa41244a17020c80c9097cc2349248f2647
Thomas Andrews <github@thomasoandrews.com>
2023-05-10 13:26:50 -0400

    Add beginning PartialHand class and a test.

---
+++ src/bridge/hand.ts
@@ -0,0 +1,37 @@
+import {Deck, Card, Rank, Suit} from './constants'
+import {Holding, XHolding, HoldingLike} from './holding'
+
+type OptionalHolding = HoldingLike | undefined
+
+type SuitTuple<T> = readonly [T,T,T,T]
+
+class PartialHand {
+    readonly holdings: SuitTuple<OptionalHolding>
+
+    constructor(holdings: SuitTuple<OptionalHolding>) {
+        this.holdings = holdings
+    }
+
+    holding(suit:Suit):OptionalHolding { return this.holdings[suit.order] }
+
+    eachHolding(callback: (suit:Suit,holding:HoldingLike) => void):void {
+        Deck.suits.all.forEach((suit:Suit) => {
+            const holding: OptionalHolding = this.holdings[suit.order]
+            if (holding) {
+                callback(suit,holding)
+            }
+        })
+    }
+
+    asString(key:"symbol"|"letter"="letter"):string {
+        const suits = new Array<string>()
+        this.eachHolding((suit:Suit,holding:HoldingLike) => {
+            suits.push((suit[key] as string)+holding.asString(''))
+        })
+        return suits.join(' ')
+    }
+
+    
+}
+
+export {PartialHand, Suit, SuitTuple, }

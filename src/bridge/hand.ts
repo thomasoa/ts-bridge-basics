598bb6e18e46d932ac6dc6aec0f26b708796b745
Thomas Andrews <github@thomasoandrews.com>
2023-05-10 14:15:34 -0400

    Addd card-related methods.

--- src/bridge/hand.ts
+++ src/bridge/hand.ts
@@ -6,6 +6,7 @@ type OptionalHolding = HoldingLike | undefined
 type SuitTuple<T> = readonly [T,T,T,T]
 
 class PartialHand {
+    static readonly voidH = new Holding(0)
     readonly holdings: SuitTuple<OptionalHolding>
     readonly length: number
 
@@ -18,12 +19,31 @@ class PartialHand {
 
     holding(suit:Suit):OptionalHolding { return this.holdings[suit.order] }
 
+    safeHolding(suit:Suit):HoldingLike { 
+        return this.holding(suit) || PartialHand.voidH 
+    }
+    
+    has(card:Card):boolean {
+        return this.safeHolding(card.suit).has(card.rank)
+    }
+
+    isSpot(card:Card):boolean {
+        return this.safeHolding(card.suit).isSpot(card.rank)
+    }
+    
     eachHolding(callback: (suit:Suit,holding:HoldingLike) => void):void {
         Deck.suits.all.forEach((suit:Suit) => {
             const holding: OptionalHolding = this.holdings[suit.order]
             if (holding) {
                 callback(suit,holding)
             }
+        })
+    }
+
+    eachCard(callback:(card:Card,isSpot:boolean) => void):void {
+        const _this = this
+        this.eachHolding((suit:Suit, h:HoldingLike) => {
+            h.ranks.forEach((rank:Rank) => callback(rank.of(suit),h.isSpot(rank)))
         })
     }
 
--- tests/bridge.hand.test.ts
+++ tests/bridge.hand.test.ts
@@ -1,9 +1,16 @@
 import {PartialHand} from "../src/bridge/hand"
 import {Holding, XHolding} from "../src/bridge/holding"
+import {Deck} from "../src/bridge/constants"
 
 test('partial hand each holding', ()=> {
+    const R = Deck.ranks
+    const S = Deck.suits
     const h = Holding.forString
     const partial = new PartialHand([h('A32'),undefined,h('KJ10'),undefined])
     expect(partial.asString()).toBe('SA32 DKJ10')
     expect(partial.length).toBe(6)
+    expect(partial.has(R.ace.of(S.spades))).toBeTruthy()
+    expect(partial.has(R.queen.of(S.hearts))).toBeFalsy()
+    expect(partial.has(R.queen.of(S.diamonds))).toBeFalsy()
+    
 })

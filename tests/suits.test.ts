567d0afad912022e8d1a87b4e52b2a46d7a2da3c
Thomas Andrews <github@thomasoandrews.com>
2023-05-10 18:01:20 -0400

    Add test for Suit.select

--- tests/suits.test.ts
+++ tests/suits.test.ts
@@ -1,5 +1,13 @@
-import { Deck } from "../src/bridge/constants"
+import { Deck, Suit, SuitTuple} from "../src/bridge/constants"
 
 test("Ensure there are four suits", () => {
     expect(Deck.suits.all.length).toBe(4)
+})
+
+test('Seat.select from SeatTuple', ()=> {
+    const st:SuitTuple<String> = ['a','b','c','d']
+    expect(Deck.suits.spades.select(st)).toBe('a')
+    expect(Deck.suits.hearts.select(st)).toBe('b')
+    expect(Deck.suits.diamonds.select(st)).toBe('c')
+    expect(Deck.suits.clubs.select(st)).toBe('d')
 })

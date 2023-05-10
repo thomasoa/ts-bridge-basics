4956acc23a307d09c546c1b3440701e8ccd8716b
Thomas Andrews <github@thomasoandrews.com>
2023-05-10 10:59:54 -0400

    Add index interface to basic library.

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import {Deck, Seats, Seat, Suit, Rank, Card } from './bridge/constants'
+import {Holding, XHolding, HoldingLike} from './bridge/holding'
+
+export {
+    Deck, Seats,            // Constants
+    Seat, Suit, Rank, Card, // types
+    Holding, XHolding,      // Classes
+    HoldingLike             // interface
+}

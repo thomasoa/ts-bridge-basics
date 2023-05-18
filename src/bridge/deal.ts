a130a837b1fc69f0357e496d8c586da087e7062d
Thomas Andrews <github@thomasoandrews.com>
2023-05-18 11:21:53 -0400

    Add first constructor for FullDeal class.

---
+++ src/bridge/deal.ts
@@ -0,0 +1,27 @@
+import {SeatTuple, SuitTuple, Card, Deck, Seat} from './constants'
+import {FullHand} from './hand'
+import {Holding} from './holding'
+
+class FullDeal {
+    hands:SeatTuple<FullHand> 
+
+    constructor(whom:Seat[]) {
+        const bits:SeatTuple<SuitTuple<number>> = [
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0]
+        ]
+        Deck.cards.each((card:Card) => {
+            const seat = whom[card.order]
+            let newBits = card.rank.bit | card.suit.value(seat.value(bits))
+            card.suit.value(seat.value(bits),newBits)
+        })
+        this.hands = bits.map((handBits)=> {
+            const holdings:SuitTuple<Holding> = handBits.map(
+                (suitBits) => new Holding(suitBits)
+            ) as SuitTuple<Holding>
+            return new FullHand(holdings)
+        }) as SeatTuple<FullHand>
+    }
+}
